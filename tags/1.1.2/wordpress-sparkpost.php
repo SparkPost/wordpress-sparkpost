e5853fb7689f9fb289a27325baaa08809839b67b
Ewan Dennis <ewandennis@noreply.github.com>
2016-01-08 12:16:25 +0000

    1.1.2 tag, readme update

---
+++ tags/1.1.2/wordpress-sparkpost.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package wp-sparkpost
+ */
+/*
+Plugin Name: SparkPost
+Plugin URI: http://sparkpost.com/
+Description: Send all your email from Wordpress through SparkPost, the world's most advanced email delivery service.
+Version: 1.1.2
+Author: The HungryCoder
+Author URI: http://thehungrycoder.com
+License: GPLv2 or later
+Text Domain: wpsp
+*/
+
+defined('ABSPATH') or die('Damn you!');
+define('WPSP_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('WPSP_PLUGIN_PATH', WPSP_PLUGIN_DIR . basename(__FILE__));
+
+require_once(WPSP_PLUGIN_DIR . 'mailer.class.php');
+require_once(WPSP_PLUGIN_DIR . 'sparkpost.class.php');
+
+new SparkPost();
+new SparkPostMailer();
+
+if (is_admin()) {
+    require_once(WPSP_PLUGIN_DIR . 'widget.class.php');
+    new SparkPostAdmin();
+} 
