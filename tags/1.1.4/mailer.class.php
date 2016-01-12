851d589e40f9ecc9951e4ce5b860a2f41f11169f
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-01-12 17:01:54 +0000

    v1.1.4 tag

---
+++ tags/1.1.4/mailer.class.php
@@ -0,0 +1,36 @@
+<?php
+// If ABSPATH is defined, we assume WP is calling us.
+// Otherwise, this could be an illicit direct request.
+if (!defined('ABSPATH')) exit();
+
+
+/**
+ * @package wp-sparkpost
+ */
+class SparkPostMailer
+{
+
+    public function __construct()
+    {
+        add_action('phpmailer_init', array($this, 'configure_phpmailer'), 2);
+    }
+
+    public function configure_phpmailer($phpmailer) {
+    	$options = get_option('sp_settings');
+
+        if(!$options['enable_sparkpost'] || empty($options['password']) || empty($options['from_email']) || empty($options['from_name'])) {
+            return;
+        }
+
+        $phpmailer->isSMTP();
+        $phpmailer->SMTPSecure = 'tls';
+        $phpmailer->Port = 587;
+        $phpmailer->Host = 'smtp.sparkpostmail.com';
+        $phpmailer->setFrom($options['from_email'], $options['from_name']);
+        $phpmailer->AddReplyTo($options['from_email'], $options['from_name']); 
+        
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Username = 'SMTP_Injection';
+        $phpmailer->Password = $options['password'];
+    }
+}
