d9e06ab7f8888e47e68e9dc9d3f7e5aacea9ada7
mhossain <mohammad.hossain@sparkpost.com>
2016-02-27 18:41:17 -0500

    Use filter to set sender name, email

--- trunk/mailer.class.php
+++ trunk/mailer.class.php
@@ -13,6 +13,8 @@ class SparkPostMailer
     public function __construct()
     {
         add_action('phpmailer_init', array($this, 'configure_phpmailer'), 2);
+        add_filter('wp_mail_from', array($this, 'set_from_email'));
+        add_filter('wp_mail_from_name', array($this, 'set_from_name'));
     }
 
     public function configure_phpmailer($phpmailer) {
@@ -26,11 +28,20 @@ class SparkPostMailer
         $phpmailer->SMTPSecure = 'tls';
         $phpmailer->Port = !empty($options['port']) ? intval($options['port']) : 587;
         $phpmailer->Host = 'smtp.sparkpostmail.com';
-        $phpmailer->setFrom($options['from_email'], $options['from_name']);
         $phpmailer->AddReplyTo($options['from_email'], $options['from_name']); 
         
         $phpmailer->SMTPAuth = true;
         $phpmailer->Username = 'SMTP_Injection';
         $phpmailer->Password = $options['password'];
+    }
+
+    public function set_from_name() {
+        $options = get_option('sp_settings');
+        return $options['from_name'];
+    }
+
+    public function set_from_email() {
+        $options = get_option('sp_settings');
+        return $options['from_email'];
     }
 }
