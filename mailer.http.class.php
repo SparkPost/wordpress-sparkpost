1caf3293cdf94ed5a864ed8f21d86a6223bc37e2
mhossain <mohammad.hossain@sparkpost.com>
2017-06-27 05:37:41 -0400

    remove errorneous code

--- mailer.http.class.php
+++ mailer.http.class.php
@@ -235,7 +235,6 @@ class SparkPostHTTPMailer extends \PHPMailer
             return false;
         }
 
-        $this->debug('Response headers: ' . print_r($response['response'], true));
         $this->debug('Response headers: ' . print_r($response['headers'], true));
         $this->debug('Response body: ' . print_r($response['body'], true));
 
